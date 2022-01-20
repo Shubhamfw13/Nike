4b1f1b253ea226ea1f306c9ae6f4ee77a9c2798d
Anands-88 <anandsholla8@gmail.com>
2022-01-20 21:44:37 +0530

    Add sales and customise files

---
+++ footer.js
@@ -0,0 +1,62 @@
+function footer()
+{
+    return ` 
+    <div id = "footer_first">
+    <div>
+        <h6>FIND A STORE</h6>
+        <h6>BECOME A MEMBER</h6>
+        <h6>SIGN UP FOR EMAIL</h6>
+        <h6>STUDENT DISCOUNTS</h6>
+        <h6>SEND US FEEDBACK</h6>
+    </div>
+    <div id = "footer_get_help">
+        <h6>GET HELP</h6>
+        <p>Order Status</p>
+        <p>Delivery</p>
+        <p>Returns</p>
+        <p>Payment Options</p>
+        <p>Contact Us ON Nike.com Inquiries</p>
+        <p>Contact Us On All Other Inquiries</p>
+    </div>
+    <div id = "footer_about">
+        <h6>ABOUT</h6>
+        <p>News</p>
+        <p>Careers</p>
+        <p>Investors</p>
+        <p>Sustainability</p>
+    </div>
+    <div id = "customise_footer_icon">
+        <button>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.KCOMqyxNd-Ph8uAwUHJ9ZwHaHa%26pid%3DApi&f=1" alt="">
+        </button>
+        <button>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qAvEb6dwa1l715giSS_qBAHaHa%26pid%3DApi&f=1" alt="">
+        </button>
+        <button>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.NXMmTT3UNjAYCL1dJNchaAAAAA%26pid%3DApi&f=1" alt="">
+        </button>
+        <button>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.aXzo-kxm-dVvhZ3DF5050QHaHa%26pid%3DApi&f=1" alt="">
+        </button>
+    </div>
+</div>
+
+<div id = footer_lastline>
+    <div id = "footer_location">
+        <div>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Xdbd6eGTSYsdiwgms62HcQHaHa%26pid%3DApi&f=1" alt="">
+        </div>
+         <P><b>India </b> Ⓒ2022 Nike,Inc. All Rights Reserved</P>
+    </div>
+    <div id = "footer_terms">
+        <p>Guides</p>
+        <p>Terms of Sale</p>
+        <p>Terms of Use</p>
+        <p>Nike Privacy Policy</p>
+    </div>
+    
+
+</div>`
+}
+
+export default footer
---
+++ sales.js
@@ -0,0 +1,81 @@
+function getdata(product,location)
+{
+    location.innerHTML = "";
+    product.forEach(elem => {
+            
+        let product = document.createElement("div")
+
+        let image_box = document.createElement("div")
+        image_box.setAttribute("id","image_box")
+
+        let image = document.createElement("img")
+        image.src = elem.image
+
+        let content_box = document.createElement("div")
+        content_box.setAttribute("id","content_box")
+
+        let name = document.createElement("h5")
+        name.textContent = elem.name
+
+        let category = document.createElement("p")
+        category.textContent = elem.category
+
+        let price = document.createElement("p")
+        price.textContent = elem.price
+
+        image_box.append(image)
+        content_box.append(name,category,price)
+        product.append(image_box,content_box)
+        location.append(product)
+    })
+}
+
+
+function filter_cat(data,main,location,name)
+{
+    let section = document.createElement("div")
+    section.style.cssText = `
+    display:flex;
+    height:35px;
+    border:1px ridge;
+    padding-top:2%`;
+
+    let button = document.createElement("button")
+    button.setAttribute("class","cat_click")
+    button.style.cssText = 
+    `margin:0% 4% 0% 5%;
+    height:20px;
+    width:20px;
+    border-radius:5px;
+    background:white;
+    border:1px solid black`;
+    let sec_name = document.createElement("span")
+    sec_name.textContent = name
+
+    button.addEventListener("click",() => 
+    {
+       let products = data.filter(function(elem)
+        {
+            let word = event.target.nextSibling.textContent
+            let include = elem.category
+            let has = include.includes(word)
+
+            if( has == true)
+            {
+                return include
+            }
+            
+            
+        })
+        let place = document.querySelector("#sale_products")
+        getdata(products,place)
+
+    })
+
+    section.append(button,sec_name)
+
+    location.append(section)
+    main.append(location)
+}
+
+export {getdata,filter_cat}
