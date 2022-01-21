e20398095e51e88dfa4878abc8577c11aad999a1
Shubhamfw13 <shubham_fw13_300@masai.school>
2022-01-21 15:32:26 +0530

    first

---
+++ js/sneakerdata.js
@@ -0,0 +1,235 @@
+var data = [
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/183c52a2-b089-4569-9819-ee5dd8e459b5/older-kids-dunk-low-halloween-do3806-100-release-date.jpg",
+    category: "Older Kid's Dunk Low",
+    name: "Halloween",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/870df7c9-91df-4714-825e-6560a0afd553/women-s-dunk-low-prime-pink-dq9324-600-release-date.jpg",
+    category: "Women's Dunk Low",
+    name: "Prime Pink",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/9d2af04e-8372-4b91-bc53-23fd95da09e1/women-s-air-jordan-5-low-girls-that-hoop-da8016-806-release-date.jpg",
+    category: "Women's Air Jordan 5 Low",
+    name: "Girls That Hoop",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/3249465b-a198-4399-8373-741d17da9935/air-jordan-1-dark-marina-blue-555088-404-release-date.jpg",
+    category: "Air Jordan 1",
+    name: "Dark Marina Blue",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/1d7777c6-0133-447e-ac4d-161fbbe70734/dunk-high-light-chocolate-release-date.jpg",
+    category: "Dunk High",
+    name: "Light Chocolate",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/40d207a2-c023-45e3-962a-dbc47a329de0/women-s-dunk-low-vintage-green-dq8580-100-release-date.jpg",
+    category: "Women's Dunk Low",
+    name: "Vintage Green",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/6ae257e1-e6a4-472a-bfb5-ba437d990f12/air-more-uptempo-96-trading-cards-release-date.jpg",
+    category: "Air More Uptempo '96",
+    name: "Trading cards",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/2b90da6c-84b8-4697-96f7-8f81969ce2cf/air-max-terrascape-90-fuel-orange-dh2973-200-release-date.jpg",
+    category: "Air Max TerraScape 90",
+    name: "Fuel Orange",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/d0fe9954-8629-4a59-b32f-1e4a7a6fd51f/women-s-dunk-high-vintage-black-dq8581-100-release-date.jpg",
+    category: "Women's Dunk High",
+    name: "Vintage Black",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/338fa63c-1898-4bce-beb9-4c80c92ea2cf/air-force-1-off-noir-dq8571-001-release-date.jpg",
+    category: "Air Force 1",
+    name: "Off Noir",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/5868ed8e-bcba-464f-b26f-a0c8bd86bda6/dunk-low-championship-court-purple-dd1391-104-release-date.jpg",
+    category: "Dunk Low",
+    name: "Championship Court Purple",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/49121efe-769b-44e1-b73e-e622bed95b81/lebron-9-king-do9353-001-release-date.jpg",
+    category: "Lebron 9",
+    name: "King",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/6e84feed-c045-48ff-af95-1d0f160de077/air-more-uptempo-release-date.jpg",
+    category: "Kids Air More Uptempo",
+    name: "White",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/fdd6963e-d29c-4a76-9546-b289b9e341c9/lebron-9-south-coast-do5838-001-release-date.jpg",
+    category: "LeBron 9",
+    name: "South Coast",
+  },
+  {
+    image:
+      "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/512d1b02-3da0-4801-9f12-14bc7e4a3bd5/sb-dunk-low-pro-dark-russet-dh1319-200-release-date.jpg",
+    category: "SB Dunk Low Pro ",
+    name: "Dark Russet",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/CT8012_005_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DC3590_100_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/CT3290_300_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DO9549_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DC9450_100_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DN5080_100_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/CZ1757_300_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DH1085_700_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/555088_611_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DN9909_200_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DM0840_200_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DM0840_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/AR0715_010_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/CT8532_030_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DH3718_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DO2701_715_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DO2704_010_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DC8891_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DN4904_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DJ9568_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+  {
+    image:
+      "https://secure-images.nike.com/is/image/DotCom/DC6083_500_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  },
+];
+
+const Feed = () => {
+  for (var i = 0; i < 15; i++) {
+    let feeddiv = document.createElement("div");
+    feeddiv.setAttribute("class", "feeddiv");
+
+    let imag = document.createElement("img");
+    imag.src = data[i].image;
+
+    let categ = document.createElement("p");
+    categ.innerHTML = data[i].category;
+    categ.setAttribute = ("class","categ")
+
+    let nam = document.createElement("p");
+    nam.innerHTML = data[i].name;
+    nam.setAttribute = ("class","nam")
+
+    let comingsoonbut = document.createElement("button")
+    comingsoonbut.setAttribute = ("class","comingsoonbutton")
+    comingsoonbut.innerHTML = "Coming Soon"
+
+    feeddiv.append(imag, categ, nam,comingsoonbut);
+
+    document.getElementById("feed-main").append(feeddiv);
+    
+  }
+};
+
+const inStock = () => {
+  data.map((elem) => {
+    let stockdiv = document.createElement("div");
+    stockdiv.setAttribute("class", "stockdiv");
+
+    let imag = document.createElement("img");
+    imag.src = elem.image;
+
+    stockdiv.append(imag);
+    document.getElementById("instock-main").append(stockdiv);
+  });
+};
+
+// function but(){
+//   document.querySelector(".feeddiv").addEventListener("click",function(){
+//     document.querySelector(".categ").style.display="none"
+//     document.querySelector(".nam").style.display="none"
+//     document.querySelector(".comingsoonbutton").style.display="block"
+//   })
+// }
+
+
+function InStockPage() {
+  window.location.href = "sneakersinstock.html";
+}
+function FeedPage() {
+  window.location.href = "sneakersfeed.html";
+}
+function UpcomingPage() {
+  window.location.href ="sneakersupcoming.html"
+}
