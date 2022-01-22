02f5ba72d301b0eeda11fb05518babffb67d163e
Anands-88 <anandsholla8@gmail.com>
2022-01-22 20:02:31 +0530

    Add comments

--- Sign up/sign_up.js
+++ Sign up/sign_up.js
@@ -5,49 +5,46 @@ let reg_obj = {
     firstname:"first",
     lastname:"last",
     dob:"birth",
-    country:"country"
+    country:"country" // Objects with dummy values
 }
 
-document.querySelector("#join_us").addEventListener("click",(event) =>
+document.querySelector("#join_us").addEventListener("click",(event) => // if clicked on sign up button 
 {
-    event.preventDefault() 
+    event.preventDefault()  /// preventing default behavior of refresh
 
-    let email =  document.querySelector("#email").value
+    let email =  document.querySelector("#email").value //Getting all entered values
     let password =  document.querySelector("#password").value
     let first =  document.querySelector("#first").value
     let last =  document.querySelector("#last").value
     let birth =  document.querySelector("#birth").value
     let country =  document.querySelector("#country").value
 
-    if(email == "" || password == "" || first == "" || last == "" || birth == "")
+    if(email == "" || password == "" || first == "" || last == "" || birth == "") // if empty or not filled any field
     {
-        alert(`Please fill all fields` )
+        alert(`Please fill all fields` ) // alert to fill all 
     
         document.querySelector("#email").value = "";
         document.querySelector("#password").value = "";
         document.querySelector("#first").value = "";
         document.querySelector("#last").value = "";
-        document.querySelector("#birth").value = "";
+        document.querySelector("#birth").value = ""; // making empty again
     }
-    else
+    else // if filled all
     {
         reg_obj["email"] = email
         reg_obj["password"] = password
         reg_obj["firstname"] = first
         reg_obj["lastname"] = last
         reg_obj["dob"] = birth
-        reg_obj["country"] = country
+        reg_obj["country"] = country // Putting details to object
 
-        localStorage.setItem("reg_data",JSON.stringify(reg_obj))
+        localStorage.setItem("reg_data",JSON.stringify(reg_obj)) // setting items to local storage
 
-        window.location.href = "./sign_in.html";
+        window.location.href = "./sign_in.html"; // next page
     }
-
-
-
 })
 
-document.querySelector("#male").addEventListener("click",() =>
+document.querySelector("#male").addEventListener("click",() => // gender button styles upon click
 {
     reg_obj["gender"] = event.target.textContent
 
