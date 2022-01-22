5e56b6561198993a50f103c8f7d516f16b99aca4
Shubhamfw13 <shubham_fw13_300@masai.school>
2022-01-22 18:59:00 +0530

    fourth commit

--- js/sneakerdata.js
+++ js/sneakerdata.js
@@ -4,90 +4,105 @@ var data = [
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/183c52a2-b089-4569-9819-ee5dd8e459b5/older-kids-dunk-low-halloween-do3806-100-release-date.jpg",
     category: "Older Kid's Dunk Low",
     name: "Halloween",
+    price: 8899
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/870df7c9-91df-4714-825e-6560a0afd553/women-s-dunk-low-prime-pink-dq9324-600-release-date.jpg",
     category: "Women's Dunk Low",
     name: "Prime Pink",
+    price: 6775
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/9d2af04e-8372-4b91-bc53-23fd95da09e1/women-s-air-jordan-5-low-girls-that-hoop-da8016-806-release-date.jpg",
     category: "Women's Air Jordan 5 Low",
     name: "Girls That Hoop",
+    price: 4654
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/3249465b-a198-4399-8373-741d17da9935/air-jordan-1-dark-marina-blue-555088-404-release-date.jpg",
     category: "Air Jordan 1",
     name: "Dark Marina Blue",
+    price: 8874
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/1d7777c6-0133-447e-ac4d-161fbbe70734/dunk-high-light-chocolate-release-date.jpg",
     category: "Dunk High",
     name: "Light Chocolate",
+    price: 3253
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/40d207a2-c023-45e3-962a-dbc47a329de0/women-s-dunk-low-vintage-green-dq8580-100-release-date.jpg",
     category: "Women's Dunk Low",
     name: "Vintage Green",
+    price: 3765
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/6ae257e1-e6a4-472a-bfb5-ba437d990f12/air-more-uptempo-96-trading-cards-release-date.jpg",
     category: "Air More Uptempo '96",
     name: "Trading cards",
+    price: 8756
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/2b90da6c-84b8-4697-96f7-8f81969ce2cf/air-max-terrascape-90-fuel-orange-dh2973-200-release-date.jpg",
     category: "Air Max TerraScape 90",
     name: "Fuel Orange",
+    price: 3453
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/d0fe9954-8629-4a59-b32f-1e4a7a6fd51f/women-s-dunk-high-vintage-black-dq8581-100-release-date.jpg",
     category: "Women's Dunk High",
     name: "Vintage Black",
+    price: 2355
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/338fa63c-1898-4bce-beb9-4c80c92ea2cf/air-force-1-off-noir-dq8571-001-release-date.jpg",
     category: "Air Force 1",
     name: "Off Noir",
+    price: 3656
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/5868ed8e-bcba-464f-b26f-a0c8bd86bda6/dunk-low-championship-court-purple-dd1391-104-release-date.jpg",
     category: "Dunk Low",
     name: "Championship Court Purple",
+    price: 5678
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/49121efe-769b-44e1-b73e-e622bed95b81/lebron-9-king-do9353-001-release-date.jpg",
     category: "Lebron 9",
     name: "King",
+    price: 7846
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/6e84feed-c045-48ff-af95-1d0f160de077/air-more-uptempo-release-date.jpg",
     category: "Kids Air More Uptempo",
     name: "White",
+    price: 3534
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/fdd6963e-d29c-4a76-9546-b289b9e341c9/lebron-9-south-coast-do5838-001-release-date.jpg",
     category: "LeBron 9",
     name: "South Coast",
+    price: 4567
   },
   {
     image:
       "https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/512d1b02-3da0-4801-9f12-14bc7e4a3bd5/sb-dunk-low-pro-dark-russet-dh1319-200-release-date.jpg",
     category: "SB Dunk Low Pro ",
     name: "Dark Russet",
+    price: 5865
   },
 ];
 
